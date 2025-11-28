6862a40331098745f75431bd1e9ba16fde0afea5
Dharun Nagavel <dharunnagavel1226@gmail.com>
2025-11-29 04:17:57 +0530

    ngrok setup

--- backend/Blockchain/hardhat.config.js
+++ backend/Blockchain/hardhat.config.js
@@ -12,6 +12,12 @@ module.exports = {
     },
     localhost: {
       url: "http://127.0.0.1:8545"
+    },
+     ngrok: {
+      url: " https://kenisha-athletic-lenora.ngrok-free.dev",  // paste your ngrok url here
+      accounts: [
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"  
+      ]
     }
   }
 };
