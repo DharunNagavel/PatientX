245dffbe34caa61910b289bce76f19eebcfe4218
Dharun Nagavel <dharunnagavel1226@gmail.com>
2025-11-29 05:10:12 +0530

    docker blockchain

--- backend/Blockchain/hardhat.config.js
+++ backend/Blockchain/hardhat.config.js
@@ -13,11 +13,11 @@ module.exports = {
     localhost: {
       url: "http://127.0.0.1:8545"
     },
-     ngrok: {
-      url: " https://kenisha-athletic-lenora.ngrok-free.dev",  // paste your ngrok url here
-      accounts: [
-        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"  
-      ]
-    }
+     docker: {
+    url: "http://127.0.0.1:8545",
+    accounts: [
+      "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+    ]
+  }
   }
 };
