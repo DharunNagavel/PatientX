db1cad49ba65f81e049e6ef5472ca5ecb09b1eb5
Dharun Nagavel <dharunnagavel1226@gmail.com>
2025-11-29 04:29:18 +0530

    neon db

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,13 @@
 import { Pool } from "pg";
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'PatientX',
-    password: 'dharun@2005',
-    port: 5432,
-});
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'PatientX',
+//     password: 'dharun@2005',
+//     port: 5432,
+// });
+
+const pool = new Pool({connectionString: "postgresql://neondb_owner:npg_flm7ehY4bcIa@ep-shiny-sound-adlk2znm-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"});
 
 export default pool;
