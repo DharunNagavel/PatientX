f4300d4b27d734c2a7479730993687b16147cb29
Dharun Nagavel <dharunnagavel1226@gmail.com>
2025-11-29 05:34:22 +0530

    render blockchain

--- backend/Blockchain/hardhat.config.js
+++ backend/Blockchain/hardhat.config.js
@@ -18,6 +18,10 @@ module.exports = {
     accounts: [
       "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
     ]
+  },
+  render: {
+    url: "https://patientx-blockchain.onrender.com",
+    accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"]
   }
   }
 };
